adfebf11fba2c916a272db82fa47d076ce510514
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2026-02-02 14:48:43 +0200

    news

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const js = document.querySelector('#js');
+const html = document.querySelector('#html');
+const content = document.querySelector('#content');
+const backdrop = document.querySelector('#backdrop');
+
+content.addEventListener('click', openCard);
+backdrop.addEventListener('click', closeModaL);
+
+// js.classList.add('open');
+// setTimeout(function() {
+//     js.classList.remove('open');
+// }, 2000)
+
+function openCard() {
+    js.classList.add('open');
+}
+function closeModaL() {
+    js.classList.remove('open');
+}
+
+function openCard() {
+    html.classList.add('open');
+}
+function closeModaL() {
+    html.classList.remove('open');
+}
+
