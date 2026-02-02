3e953cdef40ce33f3b63021c5ce3c41d0d173e2c
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2026-02-02 22:59:55 +0200

    newsa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const html = document.querySelector('#html');
+const html1 = document.querySelector('#html');
 const css = document.querySelector('#css');
 const js = document.querySelector('#js');
 const git = document.querySelector('#git');
@@ -9,10 +9,13 @@ const backdrop = document.querySelector('#backdrop');
 content.addEventListener('click', openCard);
 backdrop.addEventListener('click', closeModaL);
 
-// js.classList.add('open');
-// setTimeout(function() {
-//     js.classList.remove('open');
-// }, 2000)
+const technologies = [
+    {title: 'HTML', description: 'HTML Text', type: 'html', done: false},
+    {title: 'CSS', description: 'CSS Text', type: 'css', done: false},
+    {title: 'JavaScript', description: 'JavaScript Text', type: 'js', done: false},
+    {title: 'Git', description: 'Git Text', type: 'git', done: false},
+    {title: 'React', description: 'React Text', type: 'react', done: false},
+];
 
 function openCard() {
     js.classList.add('open');
@@ -20,6 +23,23 @@ function openCard() {
 function closeModaL() {
     js.classList.remove('open');
 }
+
+if (technologies.length === 0) {
+    content.innerHTML = '<p class="empty">Оберіть мови: HTML, CSS, JavaScript, Git, React!</p>'
+} else {
+    let html = '';
+for (let i = 0; i < technologies.length; i++) {
+    const tech = technologies[i];
+    html += `
+    <div class="card">
+        <h3>${tech.title}</h3>
+    </div>
+    `
+}
+content.innerHTML = html;
+}
+
+
 
 
 // function openCard() {
